617796d9f39320d494124af553474c840b0d9aff
Matheus Balan <matheusbalanj5@gmail.com>
2022-11-16 14:13:42 -0300

    Ajuste nas mensagens de resposta

--- Desafio/Program.cs
+++ Desafio/Program.cs
@@ -4,28 +4,29 @@ double preMedia = 0;
 try{
 Console.WriteLine("Qual a quantidade de notas deseja inserir? ");
 
-int numNotas = Convert.ToInt32(Console.ReadLine());
+    int numNotas = Convert.ToInt32(Console.ReadLine());
 
 for(int i = 1; i <= numNotas; i++){
 
-    Console.WriteLine("Digite a " + i + "º nota:");
+    Console.WriteLine($"Digite a {i}º nota:");
 
     double notas = Convert.ToDouble(Console.ReadLine());
+    
     preMedia = preMedia + notas;
 
 }
 
 double media = (preMedia) / numNotas;
-Console.WriteLine("A média do aluno foi: " + media);
+Console.WriteLine($"A média do aluno foi: {media}");
 
 if(media >= 6){
-Console.WriteLine("Aluno aprovado");
+    Console.WriteLine("Aluno aprovado");
 }else{
-Console.WriteLine("Aluno reprovado");
+    Console.WriteLine("Aluno reprovado");
 }
 
 Console.WriteLine("Deseja calcular outra média? (Digite 1 se SIM):");
-
+  
   int opcoes = Convert.ToInt32(Console.ReadLine());
 
 if(opcoes == 1)
