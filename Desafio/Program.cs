3f9fa2d2579705f1ef3657e723278d6b11b64493
Matheus Balan <114241095+Matheus-J-Balan@users.noreply.github.com>
2023-02-22 12:57:27 -0300

    Correção de identação

--- Desafio/Program.cs
+++ Desafio/Program.cs
@@ -1,41 +1,41 @@
 ﻿void calcularMedia(){
-double preMedia = 0;
 
-try{
-Console.WriteLine("Qual a quantidade de notas deseja inserir? ");
+    double preMedia = 0;
 
-    int numNotas = Convert.ToInt32(Console.ReadLine());
+    try{
+        Console.WriteLine("Qual a quantidade de notas deseja inserir? ");
 
-for(int i = 1; i <= numNotas; i++){
+        int numNotas = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine($"Digite a {i}º nota:");
+        for(int i = 1; i <= numNotas; i++){
 
-    double notas = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Digite a {i}º nota:");
+
+            double notas = Convert.ToDouble(Console.ReadLine());
+        
+            preMedia = preMedia + notas;
+
+        }
+
+        double media = (preMedia) / numNotas;
+        Console.WriteLine($"A média do aluno foi: {media}");
+
+        if(media >= 6){
+            Console.WriteLine("Aluno aprovado");
+        }else{
+            Console.WriteLine("Aluno reprovado");
+        }
+
+        Console.WriteLine("Deseja calcular outra média? (Digite 1 se SIM):");
     
-    preMedia = preMedia + notas;
+        int opcoes = Convert.ToInt32(Console.ReadLine());
 
-}
-
-double media = (preMedia) / numNotas;
-Console.WriteLine($"A média do aluno foi: {media}");
-
-if(media >= 6){
-    Console.WriteLine("Aluno aprovado");
-}else{
-    Console.WriteLine("Aluno reprovado");
-}
-
-Console.WriteLine("Deseja calcular outra média? (Digite 1 se SIM):");
-  
-  int opcoes = Convert.ToInt32(Console.ReadLine());
-
-if(opcoes == 1)
-    calcularMedia();
-}
-catch
-{
-    Console.WriteLine("Valor invalido inserido");
-    calcularMedia();
-}
+        if(opcoes == 1)
+            calcularMedia();
+        
+    }catch{
+        Console.WriteLine("Valor invalido inserido");
+        calcularMedia();
+    }
 }
 calcularMedia();    
